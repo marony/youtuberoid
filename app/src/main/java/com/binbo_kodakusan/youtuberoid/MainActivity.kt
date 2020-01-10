a2e03918e9069f2708cd34bd2b14d56a1b508843
marony <marony@binbo-kodakusan.com>
2020-01-11 01:17:56 +0900

    many changes

--- app/src/main/java/com/binbo_kodakusan/youtuberoid/MainActivity.kt
+++ app/src/main/java/com/binbo_kodakusan/youtuberoid/MainActivity.kt
@@ -5,78 +5,21 @@ import android.content.Intent.ACTION_SEND
 import android.os.AsyncTask
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kiulian.downloader.YoutubeDownloader
-import com.google.android.material.snackbar.Snackbar
+
+// EditTextにEditableを設定するために拡張メソッドで文字列から変換
+fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
 
 class MainActivity : AppCompatActivity() {
-
-    // EditTextにEditableを設定するために拡張メソッドで文字列から変換
-    fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setupControls()
-        setupWebViewForYouTube()
-
-        // 状態を復元する(インテントの処理より先にやること)
-        if (savedInstanceState != null) {
-            val editText = findViewById<EditText>(R.id.editId)
-            if (editText != null) {
-                editText.text = savedInstanceState.getString("videoIds", "").toEditable()
-            }
-        }
-
-        // インテントの処理
-        if (intent != null) {
-            println("onResume: " + intent)
-            if (intent.action == ACTION_SEND && intent.type == "text/plain") {
-                // ブラウザからの共有で呼ばれた時の処理
-                // intent: Intent { act=android.intent.action.SEND typ=text/plain flg=0x10480001 cmp=com.binbo_kodakusan.youtuberoid/.MainActivity clip={text/plain T:https://youtu.be/_VH91mivTUw} (has extras) }
-                intent.clipData?.getItemAt(0).let {
-                    val uri = it?.text
-                    val editText = findViewById<EditText>(R.id.editId)
-                    if (editText != null)
-                        editText.text = uri.toString().toEditable()
-                }
-            }
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        // 状態を保存する
-        val editText = findViewById<EditText>(R.id.editId)
-        val videoIds = editText.text.toString()
-        if (videoIds != null)
-            outState.putString("videoIds", videoIds)
-    }
-
-    private fun getVideoIds(s: String): Array<String> {
-        if (s.startsWith("http")) {
-            // "v="がついていたらその後ろから'&'の前までがid
-            // https://www.youtube.com/watch?v=_VH91mivTUw
-            val start = s.indexOf("v=")
-            if (start >= 0)
-                return arrayOf(s.substring(start + 2).substringBefore('&'))
-            // 最後の'/'の後ろがid
-            // ex) https://youtu.be/_VH91mivTUw
-            val end = s.lastIndexOf('/')
-            if (end >= 0)
-                return arrayOf(s.substring(end + 1))
-        }
-        // httpで始まっていなければidそのもの
-        // ex) _VH91mivTUw
-        return arrayOf(s)
-    }
 
     private fun setupControls() {
         // ボタンの処理
@@ -89,17 +32,32 @@ class MainActivity : AppCompatActivity() {
                 val webView = findViewById<WebView>(R.id.webView)
                 webView.loadUrl("https://www.youtube.com/watch?v=" + videoIds[0])
             }
+            val editTitle = findViewById<EditText>(R.id.editTitle)
+            editTitle.text = "ダウンロード開始します".toEditable()
             // ビデオダウンロード
             DownloadTask(this).execute(*videoIds)
         }
     }
 
     private fun setupWebViewForYouTube() {
+        val that  = this
         // WebViewの処理
         // 勝手にYouTubeアプリで開いてしまうので抑止
         val webView = findViewById<WebView>(R.id.webView)
         webView.setWebViewClient(object: WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                Log.d(this.toString(), "shouldOverrideUrlLoading: " + request?.url.toString())
+                request?.url?.let {
+                    val editText = findViewById<EditText>(R.id.editId)
+                    val oldUrl = editText.text.toString()
+                    if (it.toString() != oldUrl) {
+                        Log.d(this.toString(), oldUrl + " -> " + it.toString())
+                        editText.text = it.toString().toEditable()
+                        // タイトル取得
+                        val videoIds = getVideoIds(it.toString())
+                        GetTitleTask(that).execute(*videoIds)
+                    }
+                }
                 return false
             }
         })
@@ -108,33 +66,153 @@ class MainActivity : AppCompatActivity() {
         settings.setJavaScriptEnabled(true);
         settings.setPluginState(WebSettings.PluginState.ON)
     }
+
+    // アクティビティ作成時に呼ばれる
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupControls()
+        setupWebViewForYouTube()
+
+        // 状態を復元する(インテントの処理より先にやること)
+        savedInstanceState?.let {
+            val editText = findViewById<EditText>(R.id.editId)
+            if (editText != null) {
+                val videoIds = it.getString("videoIds", "")
+                val toast = Toast.makeText(this, "状態復元: " + videoIds, Toast.LENGTH_SHORT)
+                toast.show()
+                editText.text = videoIds.toEditable()
+            }
+        }
+
+        val that = this
+        // インテントの処理
+        intent?.let {
+            Log.d(this.toString(), "onCreate: " + it)
+            if (it.action == ACTION_SEND && it.type == "text/plain") {
+                // ブラウザからの共有で呼ばれた時の処理
+                // intent: Intent { act=android.intent.action.SEND typ=text/plain flg=0x10480001 cmp=com.binbo_kodakusan.youtuberoid/.MainActivity clip={text/plain T:https://youtu.be/_VH91mivTUw} (has extras) }
+                it.clipData?.getItemAt(0)?.let {
+                    val uri = it.text.toString()
+                    val editText = findViewById<EditText>(R.id.editId)
+                    if (editText != null) {
+                        val toast = Toast.makeText(this, "Intentから入力: " + uri, Toast.LENGTH_SHORT)
+                        toast.show()
+                        editText.text = uri.toEditable()
+                        // タイトル取得
+                        val videoIds = getVideoIds(uri)
+                        GetTitleTask(that).execute(*videoIds)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // 状態を保存する
+        val editText = findViewById<EditText>(R.id.editId)
+        val videoIds = editText.text.toString()
+        val toast = Toast.makeText(this, "状態保存: " + videoIds, Toast.LENGTH_SHORT)
+        toast.show()
+        outState.putString("videoIds", videoIds)
+    }
+
+    private fun getVideoIds(s: String): Array<String> {
+        Log.d(this.toString(), "getVideoIds: " + s)
+        if (s.startsWith("http")) {
+            // "v="がついていたらその後ろから'&'の前までがid
+            // https://www.youtube.com/watch?v=_VH91mivTUw
+            val start = s.indexOf("v=")
+            if (start >= 0) {
+                val rs = arrayOf(s.substring(start + 2).substringBefore('&'))
+                for (t in rs)
+                    Log.d(this.toString(), t)
+                return rs
+            }
+            // 最後の'/'の後ろがid
+            // ex) https://youtu.be/_VH91mivTUw
+            val end = s.lastIndexOf('/')
+            if (end >= 0) {
+                val rs = arrayOf(s.substring(end + 1))
+                for (t in rs)
+                    Log.d(this.toString(), t)
+                return rs
+            }
+        }
+        // httpで始まっていなければidそのもの
+        // ex) _VH91mivTUw
+        val rs = arrayOf(s)
+        for (t in rs)
+            Log.d(this.toString(), t)
+        return rs
+    }
 }
 
 class DownloadTask(val activity: Activity) : AsyncTask<String, String, String>() {
     override fun doInBackground(vararg params: String?): String {
-        println("doInBackground: " + params)
+        Log.d(this.toString(), "DownloadTask.doInBackground: " + params[0] + ", " + params.slice(1 until params.size))
         for (videoId: String? in params) {
-            if (videoId != null) {
-                println("videoId: " + videoId)
+            videoId?.let {
+                Log.d(this.toString(), "videoId: " + it)
+                var id: Long
                 try {
-                    val video = YoutubeDownloader.getVideo(videoId)
+                    val video = YoutubeDownloader.getVideo(it)
                     val details = video.details()
                     val formats = video.formats()
                     val format = formats.maxBy { f -> f.contentLength() }
-                    val id = video.download(format, activity)
-                    val snackbar = Snackbar.make(activity.requireViewById(R.id.btnDownload), details.title() + "をダウンロードします", Snackbar.LENGTH_SHORT)
-                    snackbar.show()
-                    println("completed: " + id)
+                    id = video.download(format, activity)
+                    Log.d(this.toString(), "completed: " + id)
                 }
                 catch (e: Throwable) {
-                    println("videoId: " + e)
+                    Log.d(this.toString(), e.toString())
+                    return "ERROR: $e"
                 }
             }
         }
-        return "success"
+        return "ダウンロード開始しました"
     }
 
     override fun onPostExecute(result: String) {
-        println("onPostExecute: " + result)
+        Log.d(this.toString(), "DownloadTask.onPostExecute: " + result)
+        if (result != "") {
+            val editTitle = activity.findViewById<EditText>(R.id.editTitle)
+            editTitle?.let {
+                editTitle.text = result.toEditable()
+            }
+        }
+    }
+}
+
+class GetTitleTask(val activity: Activity) : AsyncTask<String, String, String>() {
+    override fun doInBackground(vararg params: String?): String {
+        Log.d(this.toString(), "GetTitleTask.doInBackground: " + params[0] + ", " + params.slice(1 until params.size))
+        var title = ""
+        for (videoId: String? in params) {
+            videoId?.let {
+                Log.d(this.toString(), "videoId: " + it)
+                var id = 0L
+                try {
+                    val video = YoutubeDownloader.getVideo(it)
+                    val details = video.details()
+                    title = details.title()
+                    Log.d(this.toString(), "completed: " + id)
+                }
+                catch (e: Throwable) {
+                    Log.d(this.toString(), e.toString())
+                    return "ERROR: $e"
+                }
+            }
+        }
+        return title
+    }
+
+    override fun onPostExecute(result: String) {
+        Log.d(this.toString(), "GetTitleTask.onPostExecute: " + result)
+        if (result != "") {
+            val editTitle = activity.findViewById<EditText>(R.id.editTitle)
+            editTitle?.text = result.toEditable()
+        }
     }
 }
