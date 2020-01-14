393098aa2585e79719f00297a0283e95b334b798
marony <marony@binbo-kodakusan.com>
2020-01-14 09:32:02 +0900

    refactoring

--- app/src/main/java/com/binbo_kodakusan/youtuberoid/MainActivity.kt
+++ app/src/main/java/com/binbo_kodakusan/youtuberoid/MainActivity.kt
@@ -26,16 +26,17 @@ class MainActivity : AppCompatActivity() {
     private fun setupControls() {
         // ボタンの処理
         // ダウンロードボタン
-        val button = findViewById<Button>(R.id.btnDownload)
-        button.setOnClickListener {
+        findViewById<Button>(R.id.btnDownload)?.setOnClickListener {
             // ビデオID取得
             val videoIds = getVideoIds(findViewById<EditText>(R.id.editId).text.toString())
-            if (videoIds.size > 0) {
-                val webView = findViewById<WebView>(R.id.webView)
-                webView.loadUrl("https://www.youtube.com/watch?v=" + videoIds[0])
+            if (videoIds.isNotEmpty()) {
+                with (findViewById<WebView>(R.id.webView)) {
+                    loadUrl("https://www.youtube.com/watch?v=" + videoIds[0])
+                }
             }
-            val textTitle = findViewById<TextView>(R.id.textTitle)
-            textTitle.text = "ダウンロード開始します".toEditable()
+            with(findViewById<TextView>(R.id.textTitle)) {
+                text = "ダウンロード開始します".toEditable()
+            }
             // ビデオダウンロード
             DownloadTask(this).execute(*videoIds)
         }
@@ -43,22 +44,22 @@ class MainActivity : AppCompatActivity() {
 
     // YouTube表示用WebViewのセットアップ
     private fun setupWebViewForYouTube() {
-        val that  = this
         // WebViewの処理
         // 勝手にYouTubeアプリで開いてしまうので抑止
         val webView = findViewById<WebView>(R.id.webView)
         webView.setWebViewClient(object: WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                Log.d(this.toString(), "shouldOverrideUrlLoading: " + request?.url.toString())
-                request?.url?.let {
-                    val editText = findViewById<EditText>(R.id.editId)
-                    val oldUrl = editText.text.toString()
-                    if (it.toString() != oldUrl) {
-                        Log.d(this.toString(), oldUrl + " -> " + it.toString())
-                        editText.text = it.toString().toEditable()
-                        // タイトル取得
-                        val videoIds = getVideoIds(it.toString())
-                        GetTitleTask(that).execute(*videoIds)
+                Log.d(this@MainActivity.toString(), "shouldOverrideUrlLoading: " + request?.url.toString())
+                request?.url?.toString()?.also { uri ->
+                    with(findViewById<EditText>(R.id.editId)) {
+                        val oldUrl = text.toString()
+                        if (uri != oldUrl) {
+                            Log.d(this@MainActivity.toString(), "$oldUrl -> $uri")
+                            text = uri.toEditable()
+                            // タイトル取得
+                            val videoIds = getVideoIds(uri)
+                            GetTitleTask(this@MainActivity).execute(*videoIds)
+                        }
                     }
                 }
                 return false
@@ -73,35 +74,31 @@ class MainActivity : AppCompatActivity() {
     // アプリケーションの状態を復元
     private fun restoreStateFromBundle(savedInstanceState: Bundle?) {
         // 状態を復元する(インテントの処理より先にやること)
-        IntentUtil.getStateFromBundle(savedInstanceState)?.let {
-            val editText = findViewById<EditText>(R.id.editId)
-            if (editText != null) {
-                val toast = Toast.makeText(this, "状態復元: " + it, Toast.LENGTH_SHORT)
+        IntentUtil.getStateFromBundle(savedInstanceState)?.also { state ->
+            with(findViewById<EditText>(R.id.editId)) {
+                val toast = Toast.makeText(this@MainActivity, "状態復元: $state", Toast.LENGTH_SHORT)
                 toast.show()
-                editText.text = it.videoIds.toEditable()
+                text = state.videoIds.toEditable()
             }
         }
     }
 
     // 他のアプリケーションから送られたインテントを処理
     private fun setStateFromOtherApplication() {
-        val that = this
         // インテントの処理
-        intent?.let {
-            Log.d(this.toString(), "onCreate: " + it)
-            if (it.action == ACTION_SEND && it.type == "text/plain") {
+        intent?.also { intent ->
+            Log.d(this.toString(), "onCreate: $intent")
+            if (intent.action == ACTION_SEND && intent.type == "text/plain") {
                 // ブラウザからの共有で呼ばれた時の処理
                 // intent: Intent { act=android.intent.action.SEND typ=text/plain flg=0x10480001 cmp=com.binbo_kodakusan.youtuberoid/.MainActivity clip={text/plain T:https://youtu.be/_VH91mivTUw} (has extras) }
-                it.clipData?.getItemAt(0)?.let {
-                    val uri = it.text.toString()
-                    val editText = findViewById<EditText>(R.id.editId)
-                    if (editText != null) {
-                        val toast = Toast.makeText(this, "Intentから入力: " + uri, Toast.LENGTH_SHORT)
+                intent.clipData?.getItemAt(0)?.text?.toString()?.also { uri ->
+                    with(findViewById<EditText>(R.id.editId)) {
+                        val toast = Toast.makeText(this@MainActivity, "Intentから入力: $uri", Toast.LENGTH_SHORT)
                         toast.show()
-                        editText.text = uri.toEditable()
+                        text = uri.toEditable()
                         // タイトル取得
                         val videoIds = getVideoIds(uri)
-                        GetTitleTask(that).execute(*videoIds)
+                        GetTitleTask(this@MainActivity).execute(*videoIds)
                     }
                 }
             }
@@ -111,11 +108,12 @@ class MainActivity : AppCompatActivity() {
     // アプリケーションの状態を保存
     private fun saveStateToBundle(outState: Bundle) {
         // 状態を保存する
-        val editText = findViewById<EditText>(R.id.editId)
-        val videoIds = editText.text.toString()
-        val toast = Toast.makeText(this, "状態保存: " + videoIds, Toast.LENGTH_SHORT)
-        toast.show()
-        IntentUtil.setStateToBundle(InstanceState(videoIds), outState)
+        with(findViewById<EditText>(R.id.editId)) {
+            val videoIds = text.toString()
+            val toast = Toast.makeText(this@MainActivity, "状態保存: " + videoIds, Toast.LENGTH_SHORT)
+            toast.show()
+            IntentUtil.setStateToBundle(InstanceState(videoIds), outState)
+        }
     }
 
     // アクティビティ作成時に呼ばれる
@@ -136,7 +134,7 @@ class MainActivity : AppCompatActivity() {
 
     // URL(かvideoIdそのもの)からvideoIdを取得
     private fun getVideoIds(s: String): Array<String> {
-        Log.d(this.toString(), "getVideoIds: " + s)
+        Log.d(this.toString(), "getVideoIds: $s")
         if (s.startsWith("http")) {
             // "v="がついていたらその後ろから'&'の前までがid
             // https://www.youtube.com/watch?v=_VH91mivTUw
